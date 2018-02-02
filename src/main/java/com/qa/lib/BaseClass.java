d273b2f4476b98f76889a0b2e3214c7ea0c992a3
sanjaynaikm <34195235+sanjaynaikm@users.noreply.github.com>
2018-02-03 02:45:31 +0530

    Update BaseClass.java

--- src/main/java/com/qa/lib/BaseClass.java
+++ src/main/java/com/qa/lib/BaseClass.java
@@ -6,7 +6,7 @@ public class BaseClass
 	public static void main(String[] args) 
 	{
 		System.out.println("Base Class");
-
+		System.out.println("This was done from git. To understand PULL");
 	}
 
 }
