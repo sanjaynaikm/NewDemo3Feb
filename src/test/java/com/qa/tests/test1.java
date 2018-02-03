60c1e89bd986d1178d6d79baac293764568b6f68
sanjaynaikm <sanjaynaikm@gmail.com>
2018-02-03 12:37:29 +0530

    Changed by Git_User1 test1.java

--- src/test/java/com/qa/tests/test1.java
+++ src/test/java/com/qa/tests/test1.java
@@ -5,5 +5,6 @@ public class test1
 	public void loginTest()
 	{
 		System.out.println("Base_Login Test");
+		System.out.println("Edited to understand git merge conflict - changed by Git_User1");
 	}
 }
